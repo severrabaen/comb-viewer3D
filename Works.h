e9d3eba83402aa524a8ced6df9835fe73290dfe7
severrabaen <39644970+severrabaen@users.noreply.github.com>
2018-09-15 21:15:45 +0900

    Update Works.h

--- Works.h
+++ Works.h
@@ -2,15 +2,18 @@
 #include <Siv3D.hpp>
 #include <HamFramework.hpp>
 
+//Time which decide when the visitor not control Comb-Viewer3D for a while.
 const int disappMAndRec=10000;
 
 struct works{
+  //Viewed model and its data.
   Model workmodel;
   String titleName,authorName;
 }
 
 class Works :public MyApp::Scene{
   private:
+  //To change the model.
   Triangle goToLeft, goToRight;
   public:
 }
