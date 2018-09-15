805276f02a3f12df5fe43652962b0b89ec880883
severrabaen <39644970+severrabaen@users.noreply.github.com>
2018-09-15 21:46:50 +0900

    Update Works.cpp

--- Works.cpp
+++ Works.cpp
@@ -2,8 +2,21 @@
 #include <HamFramework.hpp>
 #include <Works.h>
 
-Works::Works{
-TextReader listReader(U"Works//Workslist.txt");
-  String fileName;
+Works::Works(const InitData& init) :IScene(init){
+  TextReader reader(U"Works//Workslist.txt");
+  String line;
+  while(reader.readLine(line)){
   
+  }
+}
+
+void Works::update(){
+
+}
+
+void Works::draw(){
+  if(!disappFlag){
+    goToRight.draw();
+    goToLeft.draw();
+  }
 }
