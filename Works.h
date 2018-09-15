9cdd798a4f8b8b848b05dd08f7f111f0bc008255
severrabaen <39644970+severrabaen@users.noreply.github.com>
2018-09-15 20:54:35 +0900

    Create Works.h

---
+++ Works.h
@@ -0,0 +1,12 @@
+#include <Siv3D.hpp>
+#include <HamFramework.hpp>
+
+struct works{
+  Model workmodel;
+  String titleName,authorName;
+}
+
+class Works :public MyApp::Scene{
+  private:
+  public:
+}
