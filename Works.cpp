ddd202d020253ea53da525055e3b49e9b2169c93
severrabaen <39644970+severrabaen@users.noreply.github.com>
2018-09-17 17:14:58 +0900

    Update Works.cpp

--- Works.cpp
+++ Works.cpp
@@ -2,21 +2,26 @@
 #include <HamFramework.hpp>
 #include <Works.h>
 
-Works::Works(const InitData& init) :IScene(init){
-  TextReader reader(U"Works//Workslist.txt");
-  String line;
-  while(reader.readLine(line)){
-  
-  }
+Works::Works(const InitData& init) :IScene(init) {
+	TextReader reader(U"Works//Workslist.txt");
+	String line;
+	while (reader.readLine(line)) {
+		workModel = Model(U"" + U".obj");
+	}
+	WorksFont = Font()//unknown;
 }
 
-void Works::update(){
-
+void Works::update() {
+	Graphics3D::FreeCamera();
 }
 
-void Works::draw(){
-  if(!disappFlag){
-    goToRight.draw();
-    goToLeft.draw();
-  }
+void Works::draw() {
+	if (!disappFlag) {
+		goToRight.draw();
+		goToLeft.draw();
+	}
+	workModel.draw();
+
+	//drawLeadLine(Window::Center(), Circular(100.0, 60_deg), 150.0, L"引き出し線", LeadLineTextHAlign::Right, LeadLineTextVAlign::Top, font);
+	//drawLeadLine(Window::Center() + Circular(50.0, -45_deg), Circular(100.0, -45_deg), 150.0, L"左にも引ける", LeadLineTextHAlign::Left, LeadLineTextVAlign::Top, font);
 }
