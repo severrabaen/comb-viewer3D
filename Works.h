e8c3a1cce7200da7ee5c9337d4af0a3b837e4df1
severrabaen <39644970+severrabaen@users.noreply.github.com>
2018-09-17 19:45:13 +0900

    Update Works.h

--- Works.h
+++ Works.h
@@ -46,7 +46,7 @@ void drawLeadLine(const Vec2& from, const Circular& lead1, const double lead2, c
 
 class Works :public MyApp::Scene {
 private:
-	Texture howToTexture;
+	Texture howToTexture,TwitterImg;
 	//To change the model(2 things).
 	Triangle goToLeft, goToRight;
 	int nowdis, nextdis, prevdis;
