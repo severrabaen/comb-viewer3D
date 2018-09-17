3258bdd265822b9b83c7d979b19fab4a09943a72
severrabaen <39644970+severrabaen@users.noreply.github.com>
2018-09-17 19:14:52 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,26 +6,27 @@
 #include "3Dviewer.h"
 
 //フルスクリーン化
-namespace fullscreen{
-constexpr Size BaseSize(720, 720);
- void setupfullsc(){
-   Window::SetBaseSize(BaseSize);
-   const auto[displayIndex, displayMode] = OptimalScreen::Get(OptimalScreen::Preference::AspectMin, BaseSize);
- 		Graphics::SetFullScreen(true, displayMode.size, displayIndex, displayMode.refreshRateHz);
- 		Print << U"Display {} | size {} @ {} Hz"_fmt(displayIndex, displayMode.size, displayMode.refreshRateHz);
- }
-	
+namespace fullscreen {
+	constexpr Size BaseSize(720, 720);
+	void setupfullsc() {
+		Window::SetBaseSize(BaseSize);
+		const auto[displayIndex, displayMode] = OptimalScreen::Get(OptimalScreen::Preference::AspectMin, BaseSize);
+		Graphics::SetFullScreen(true, displayMode.size, displayIndex, displayMode.refreshRateHz);
+		Print << U"Display {} | size {} @ {} Hz"_fmt(displayIndex, displayMode.size, displayMode.refreshRateHz);
+	}
+}
+
 void Main() {
 	App::setupfullsc();
-        Graphics::SetBackGround(Palette::Black);
+	Graphics::SetBackGround(Palette::Black);
 	Window::Resize({ windowWidth, windowHeight }, true);
-	Window::SetTitle(U"Comb Viewer 3D",verstr);
+	Window::SetTitle(U"Comb Viewer 3D", verstr);
 
 	MyApp SceneMgr;
 	//メニュー
 	SceneMgr.add<Menu>(U"Menu");
-        //本編
-	SceneMgr.add<U"3Dviewer">;
+	//本編
+	SceneMgr.add<U"Works">;
 
 	while (System::Update()) {
 		SceneMgr.updateScene();
